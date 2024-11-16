db313ff32d6f637b7b6d11b408d83a091e2e2322
zykov_valera <zykov_valera@hill.cs.msu.ru>
2024-11-17 00:57:22 +0300

    Fix

--- OCR/line_OCR/ctc/main_line_ctc.py
+++ OCR/line_OCR/ctc/main_line_ctc.py
@@ -175,7 +175,7 @@ if __name__ == "__main__":
             "use_ddp": False,  # Use DistributedDataParallel
             "use_apex": False,  # Enable mix-precision with apex package
             "nb_gpu": torch.cuda.device_count(),
-            "batch_size": 64 if torch.cuda.get_device_name() == 'NVIDIA A100-SXM4-80GB' else 16,  # mini-batch size per GPU
+            "batch_size": 128 if torch.cuda.get_device_name() == 'NVIDIA A100-SXM4-80GB' else 16,  # mini-batch size per GPU
             "optimizer": {
                 "class": Adam,
                 "args": {
